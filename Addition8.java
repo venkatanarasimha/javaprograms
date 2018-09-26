5bb344f8909a1139112a1dbcc17ad936ea29811b
venkatanarasimha <43570484+venkatanarasimha@users.noreply.github.com>
2018-09-26 16:50:48 +0530

    Add files via upload

---
+++ Addition8.java
@@ -0,0 +1,12 @@
+class Addition8{
+void add(double a,double b){
+double c = a+b;
+System.out.println(c);
+}
+public static void main(String[]args){
+Addition8 ad = new Addition8();
+double x = 1.3;
+double y = 1.3;
+ad.add(x,y);
+}
+}
